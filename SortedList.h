cc2da7994d41dd0195f8f1d7585ca17b0fdf5bcd
guy friedman <guy.friedman2001@gmail.com>
2025-01-03 12:45:29 +0200

    add comments

--- SortedList.h
+++ SortedList.h
@@ -8,59 +8,125 @@ namespace mtm {
 
     template <typename T>
     class SortedList {
+        //TODO maybe delete these comment lines
+        //provides the de-facto interface for interacting with SortedListNode,
+        //wrapping function provides flexibility with static like parameters such as
+        //head and tail, without forcing a single instance of the list class.
         template <typename T>
         class SortedListNode {
-            private:
+            //TODO maybe delete these comment lines
+            //the members of this class are only accessible by SortedList class
+        private:
             friend class SortedList<T>;
             SortedListNode<T> * prev;
             SortedListNode<T> * next;
             T* data;
-            void add(SortedListNode<T> * newNode) {
-                if (newNode > this->next){ //TODO create > operator, make null node smallest;
-                    this->next->add(newNode);
-                    return;
-                }
-                this->next->prev = newNode;
-                newNode->next = this->next;
-                this->next = newNode;
-                newNode->prev = this;
-            }
-            SortedListNode(SortedListNode<T>* prev = nullptr, SortedListNode<T>* next = nullptr, T* data = nullptr) : prev(prev), next(next), data(data) {}
-            public:
-            void add(T const& const data) {
-                if (data == nullptr) {
-                    //TODO invalid argument
-                }
-                SortedListNode<T> * newNode = new SortedListNode<T>(data);
-                this->add(newNode);
-            }
+
             SortedListNode(): prev(nullptr), next(nullptr), data(nullptr) {}
+
+            SortedListNode(T data): prev(nullptr), next(nullptr) {
+                this->data = new T(data);
+            }
+
             ~SortedListNode() {
                 this->prev = nullptr;
                 this->next = nullptr;
                 delete this->data;
             }
 
-            bool isSorted(){ //FIXME make it so the head itself wont call this function, because otherwise it would break the logic
-                if (this->next == nullptr){
+            inline bool isTail() const {
+                return (this->data == nullptr)&&(this->prev != nullptr)&&(this->next == nullptr);
+            }
+
+            inline bool isHead() const {
+                return (this->data == nullptr)&&(this->next != nullptr)&&(this->prev == nullptr);
+            }
+
+            inline bool hasNext()   const {
+                return !(this->next->isTail());
+            }
+
+            void addImmediate(SortedListNode<T>* newNode) {
+                assert(!this->isTail());
+                this->next->prev = newNode;
+                newNode->next = this->next;
+                this->next = newNode;
+                newNode->prev = this;
+            }
+
+            void add(SortedListNode<T>* newNode) {
+                //if we arrived at the end of the chain
+                if (!this->hasNext()) {
+                    this->addImmediate(newNode);
+                    assert(this->isSorted());
+                    return;
+                }
+                //if we arrived at the orderly place of the new node
+                if (newNode > this->next) {
+                    this->addImmediate(newNode);
+                    assert(this->isSorted());
+                    return;
+                }
+                //if we are yet to find the place for the new node nor did we arrive to the end of the chain
+                this->next->add(newNode);
+                assert(this->isSorted());
+            }
+
+            SortedListNode(SortedListNode<T>* prev = nullptr, SortedListNode<T>* next = nullptr, T* data = nullptr) :
+            prev(prev), next(next), data(data) {}
+
+            /**
+             * overload of the isSorted function, to differentiate calls between
+             * the head and the rest of the chain
+             *
+             * @return if the list is sorted
+             */
+            bool isSorted(int) const {
+                if (this->isTail()){
                     return true;
                 }
-                return (this >= this->next) && (this->next->isSorted()); //TODO create >= operator
+                return (this >= this->next) && (this->next->isSorted(0)); //TODO create >= operator or < and use (!(this < this->next))
+            }
+
+        public:
+            void insert(T const& const data) {
+                if (data == nullptr) {
+                    //TODO invalid argument
+                }
+                SortedListNode<T> * newNode = new SortedListNode<T>(data);
+                this->add(newNode);
+                assert(this->isSorted());
+            }
+
+            /**
+             *check if the list is sorted
+             *
+             * @return if the list is sorted
+             */
+            bool isSorted() const {
+                //the head breaks the sorted logic, if it is the head- skip it.
+                return (this->isHead())?this->next->isSorted(0):this->isSorted(0);
             }
         };
-        private:
+    private:
         int length;
         SortedListNode<T>* head;
         SortedListNode<T>* tail;
 
+        /**
+         * //maybe can delete this function
         void addInitial(SortedListNode<T>* addThis){
             assert(this->isEmpty());
             this->head->add(addThis);
         }
+         */
 
+
+        /** //maybe can delete this function
         void add(SortedListNode<T>* addThis){
             if(this->isEmpty()){
-                this->addInitial(addThis);
+                this->head->addImmediate(addThis);
+                return;
             }
             for (SortedListNode<T>* node : this){ //FIXME adjust to work correctly with the iterator
                 if (addThis > node){
@@ -69,22 +135,43 @@ namespace mtm {
                 }
             }
         }
+         *
+         */
 
-        bool isSorted(){
+
+        bool isSorted() const {
             if (this->isEmpty()){
                 return true;
             }
             return head->isSorted();
         }
 
+        bool verifyLength() const {
+            int actualLength = 0;
+            for (SortedListNode<T>* node : this) {
+                ++actualLength;
+            }
+            return actualLength == this->length;
+        }
+
+        bool verifyList() const {
+            return (this->isSorted())&&(this->verifyLength());
+        }
+
+        inline bool isEmpty(){
+            return this->length == 0;
+        }
+
     public:
-        SortedList() : length(0){
+
+        SortedList<T>() : length(0){
             this->head = new SortedListNode<T>();
             this->tail = new SortedListNode<T>();
             this->head->next = this->tail;
             this->tail->prev = this->head;
         }
-        SortedList(const SortedList& other) : length(0){
+
+        SortedList<T>(const SortedList& other) : length(0){
             if(other.head == nullptr) {
                 this->head = nullptr;
                 this->tail = nullptr;
@@ -108,13 +195,6 @@ namespace mtm {
             //     this->tail = currentNode;
             //     previousNode = currentNode;
             //     currentOther = currentOther->next;
-        }
-        
-        /**
-         * no need to keep such basic utility private
-         */
-        inline bool isEmpty(){
-            return this->length == 0;
         }
 
         SortedList<T>& operator=(const SortedList& other) {
@@ -163,6 +243,13 @@ namespace mtm {
             }
         }
 
+        void insert(const T& newElement) {
+            this->head->insert(newElement);
+            ++this->length;
+            assert(this->verifyList());
+        }
+
+        /**
         void insert(const T& newElement){
             if (newElement == nullptr) {
                     //TODO invalid argument
@@ -172,10 +259,13 @@ namespace mtm {
                 this->tail = this->head;
                 this->length = 1;
                 return;
-            }  
+            }
             this->head->add(newElement);
             this->length++;
         }
+         */
+
+
 
         int length() const {
             return this->length;
